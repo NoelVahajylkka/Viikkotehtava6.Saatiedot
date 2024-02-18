101e06f04bce53f74f953e3a67fb97fbf9333a15
NoelVahajylkka <t1vano00@students.oamk.fi>
2024-02-18 20:54:15 +0200

    tehtävä valmis

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StyleSheet, Text, View } from 'react-native';
+import Position from './components/Position';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.heading}>Current weather</Text>
+      <Position/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 50,
+  },
+  heading: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+});
